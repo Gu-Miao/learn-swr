afce026f583e22beb8beca3a1136f76cbcbfca03
Gu-Miao <1450206741@qq.com>
2023-07-18 11:33:42 +0800

    Add a easy test

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,14 @@
 import Koa from 'koa'
+import Router from '@koa/router'
 
 const app = new Koa()
+const router = new Router()
 const port = 8888
 
+router.get('/api1', async ctx => {
+  ctx.body = 1111
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,18 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+import useSWR from 'swr'
+
 function Home() {
-  return <div>Home</div>
+  const { data, error, isLoading } = useSWR<string, string>('/api', axios)
+
+  console.log(data, error)
+
+  return (
+    <div>
+      <h1>Home</h1>
+      {isLoading ? <div>Loading...</div> : error ? <div>Error</div> : <div>data: Yes</div>}
+    </div>
+  )
 }
 
 export default Home
